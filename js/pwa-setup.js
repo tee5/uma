dc8eac7579568ac42b12ba8929e8879508537f2d
tee5 <noppe18@gmail.com>
2021-03-08 02:23:07 +0900

    PWA対応の修正

--- js/pwa-setup.js
+++ js/pwa-setup.js
@@ -1,7 +1,7 @@
-$(function () {
+document.addEventListener("DOMContentLoaded", e => {
     //Service Workerの登録
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js')
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("sw.js")
         .then(function (registration) {
             if (typeof registration.update == 'function') {
                 registration.update();
