2f32db2b36a4d0f3ba8c179310bd9f7e47ea278a
tee5 <noppe18@gmail.com>
2021-03-08 03:10:46 +0900

    PWA微調整

--- js/pwa-setup.js
+++ js/pwa-setup.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", e => {
     //Service Workerの登録
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.register("js/sw.js")
-        .then(function (registration) {
+        .then(registration => {
             if (typeof registration.update == 'function') {
                 registration.update();
             }
         })
-        .catch(function (error) {
+        .catch(error => {
             console.log("Error Log: " + error);
         });
     }
